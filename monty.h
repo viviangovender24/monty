5d934889c85ce5d399a9f19cb268b6616520ae41
vivian <vivian.govender@live.com>
2023-06-21 08:05:17 -0700

    0

--- monty.h
+++ monty.h
@@ -52,7 +52,19 @@ typedef struct instruction_s
  * Description: doubly linked list node structure
  * for stack, queues, LIFO, FIFO.
  */
+typedef struct globals
+{
+	int lifo;
+	unsigned int current;
+	char *arg;
+	stack_t *head;
+	FILE *fd;
+	char *buffer;
+} global_t;
 
+extern global_t glob_v;
+
+/**opcode instructions*/
 void push(stack_t **stack, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number);
 void pint(stack_t **head, unsigned int nline);
@@ -66,4 +78,25 @@ void stack(stack_t **head, unsigned int nline);
 void _div(stack_t **head, unsigned int nline);
 void mul(stack_t **head, unsigned int nline);
 void mod(stack_t **head, unsigned int nline);
+void pstr(stack_t **head, unsigned int nline);
+void pchar(stack_t **head, unsigned int nline);
+void rotr(stack_t **head, unsigned int nline);
+void rotl(stack_t **head, unsigned int nline);
+
+
+/**get function*/
+void(*opcodes_get(char *opcode))(stack_t **stack, unsigned int line_number);
+
+/**doubly linked list functions*/
+stack_t *add_dnodeint(stack_t **head, const int n);
+stack_t *add_dnodeint_end(stack_t **head, const int n);
+void free_dlistint(stack_t *head);
+
+/**str functions */
+int _sch(char *s, char c);
+char *_strtoky(char *s, char *d);
+int _strcmp(char *s1, char *s2);
+
+/* main*/
+void free_glob_v(void);
 #endif
