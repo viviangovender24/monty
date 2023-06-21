3609ee8bba6e75337ee9cf7e6f5086d00628746f
vivian <vivian.govender@live.com>
2023-06-21 08:03:22 -0700

    0

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
-# Monty Interpreter
-This is a language interpreter built in the C programming language.
-The interpreter can run and manage both stacks(LIFO) and queues(FIFO) printing, mathematical operations and more.
+readme for monty project
--- monty.h
+++ monty.h
@@ -37,21 +37,22 @@ typedef struct stack_s
 typedef struct instruction_s
 {
 	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
+/**
+ * struct globals - global structure to use in the functions
+ * @lifo: is stack or queue
+ * @current: current line
+ * @arg: second parameter inside the current line
+ * @head: doubly linked list
+ * @fd: file descriptor
+ * @buffer: input text
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO.
+ */
 
-typedef struct glob
-{
-	int lifo;
-	unsigned int current;
-	char *arg;
-	stack_t *head;
-	FILE *fd;
-	char *buffer;
-} global_t;
-extern global_t glob_v;
-
-/**opcodes*/
 void push(stack_t **stack, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number);
 void pint(stack_t **head, unsigned int nline);
@@ -61,24 +62,8 @@ void nop(stack_t **head, unsigned int nline);
 void add(stack_t **head, unsigned int nline);
 void sub(stack_t **head, unsigned int nline);
 void queue(stack_t **head, unsigned int nline);
+void stack(stack_t **head, unsigned int nline);
 void _div(stack_t **head, unsigned int nline);
 void mul(stack_t **head, unsigned int nline);
 void mod(stack_t **head, unsigned int nline);
-
-
-/**get function*/
-void(*opcodes_get(char *opcode))(stack_t **stack, unsigned int line_number);
-
-/**doubly linked list functions*/
-stack_t *add_dnodeint(stack_t **head, const int n);
-stack_t *add_dnodeint_end(stack_t **head, const int n);
-void free_dlistint(stack_t *head);
-
-/**str functions */
-int _sch(char *s, char c);
-char *_strtoky(char *s, char *d);
-int _strcmp(char *s1, char *s2);
-
-/* main*/
-void free_glob_v(void);
 #endif
